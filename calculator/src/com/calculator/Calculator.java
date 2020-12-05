fe0ed6e5da0379e74c73c4ba93a18d9cfc0acf77
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-05 12:06:11 -0800

    Created Calculator class with a few Member variables and methods

---
+++ calculator/src/com/calculator/Calculator.java
@@ -0,0 +1,36 @@
+package com.calculator;
+
+import java.util.ArrayList;
+
+public class Calculator {
+	private ArrayList<Double> save;
+	private String Operation;
+	private double OperandOne;
+	private double OperandTwo;
+	
+	public void setOperation(String op) {
+		Operation = op;
+	}
+	public void setOperandOne(double num) {
+		OperandOne = num;
+	}
+	public void setOperandTwo(double num) {
+		OperandTwo = num;
+	}
+	public void performOperation() {
+		if (this.Operation == "+") {
+			double x = this.OperandOne + this.OperandTwo;
+			save.add(x);
+		}
+		else if (this.Operation == "-") {
+			double x = this.OperandOne - this.OperandTwo;
+			save.add(x);
+		}
+		else {
+			System.out.println("The Operation must be '+' or '-'!");
+		}
+	}
+	public String getResults() {
+		return "Most Recent Problem: " + this.OperandOne + " " + Operation + " " + this.OperandTwo + " = " + save.get(save.size() - 1);
+	}
+}
